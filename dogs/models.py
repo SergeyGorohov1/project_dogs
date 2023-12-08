ab7ad3eaf431bab8be6abcb1080aa5d10c81ced9
Сергей <goroxov.99@mail.ru>
2023-12-08 14:44:00 +0300

    add model Breed (порода)

--- dogs/models.py
+++ dogs/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 NULLABLE = {"blank": True, "null": True}
 
+
+class Breed(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Порода"
+        verbose_name_plural = "Породы"
+
+
 class Dogs(models.Model):
     name = models.CharField(max_length=100, verbose_name="Кличка")
-    breed = models.CharField(max_length=100, verbose_name="Порода")
+    breed = models.ForeignKey(Breed, on_delete=models.SET_NULL, **NULLABLE, verbose_name="Порода")
     photo = models.ImageField(upload_to="dogs/", **NULLABLE, verbose_name="Фото")
     birth_day = models.DateField(**NULLABLE, verbose_name="Дата рождения")
-
 
     def __str__(self):
         return f"{self.name} {self.breed}"
