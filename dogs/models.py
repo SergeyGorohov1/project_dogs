6cffab68f0be0614d106ad8f90619431b87f8cff
Сергей <goroxov.99@mail.ru>
2023-12-08 14:34:32 +0300

    add models dogs, makemigrations

--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+class Dogs(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Кличка")
+    breed = models.CharField(max_length=100, verbose_name="Порода")
+    photo = models.ImageField(upload_to="dogs/", **NULLABLE, verbose_name="Фото")
+    birth_day = models.DateField(**NULLABLE, verbose_name="Дата рождения")
+
+
+    def __str__(self):
+        return f"{self.name} {self.breed}"
+
+    class Meta:
+        verbose_name = "Собака"
+        verbose_name_plural = "Собаки"
