aff555fd08d98ef8cc2ffd86a092bbf1f359b2a3
Сергей <goroxov.99@mail.ru>
2023-12-08 19:02:12 +0300

    Настроен функционал просмотра всех пород

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from dogs.views import index
+from dogs.views import index, breeds
+from dogs.apps import DogsConfig
+
+app_name = DogsConfig.name
 
 urlpatterns = [
-    path("", index)
+    path("", index, name="index"),
+    path("breeds/", breeds, name="breeds"),
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -3,6 +3,13 @@ from dogs.models import Dog, Breed
 
 def index(request):
     context = {
+        "object_list": Breed.objects.all()[:3]
+    }
+    return render(request, "dogs/index.html", context)
+
+
+def breeds(request):
+    context = {
         "object_list": Breed.objects.all()
     }
     return render(request, "dogs/index.html", context)
