50f2a2a41bb909310726a4a4fddf7ef9a08e02ef
Сергей <goroxov.99@mail.ru>
2023-12-08 14:53:53 +0300

    add models in admin; add filter, search, list_display in admin

--- dogs/admin.py
+++ dogs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from dogs.models import Dog, Breed
+
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = ("name", "breed")
+    list_filter = ("name", "breed", "birth_day")
+    search_fields = ("name", "breed")
+
+
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    pass
