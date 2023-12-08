dcae3b23be1ab9d9ddca1665bd3c57a5ab78a565
Сергей <goroxov.99@mail.ru>
2023-12-08 19:31:23 +0300

    edit controllers FBV -> CBV

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from dogs.views import index, breeds, dogs_breeds
+from dogs.views import index, BreedView, DogView
 from dogs.apps import DogsConfig
 
 app_name = DogsConfig.name
 
 urlpatterns = [
     path("", index, name="index"),
-    path("breeds/", breeds, name="breeds"),
-    path("dogs/<int:pk>/", dogs_breeds, name="dogs_breeds"),
+    path("breeds/", BreedView.as_view(), name="breeds"),
+    path("dogs/<int:pk>/", DogView.as_view(), name="dogs_breeds"),
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from dogs.models import Dog, Breed
+
 
 def index(request):
     context = {
         "object_list": Breed.objects.all()[:3]
     }
-    return render(request, "dogs/index.html", context)
+    return render(request, "dogs/breed_list.html", context)
 
 
-def breeds(request):
-    context = {
-        "object_list": Breed.objects.all()
-    }
-    return render(request, "dogs/index.html", context)
+class BreedView(ListView):
+    model = Breed
 
-def dogs_breeds(request, pk):
-    context = {
-        "object_list": Dog.objects.filter(breed__pk=pk)
-    }
-    return render(request, "dogs/dogs.html", context)
+
+class DogView(ListView):
+    model = Dog
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(breed__pk=self.kwargs.get("pk"))
+        return queryset
