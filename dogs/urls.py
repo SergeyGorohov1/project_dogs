5ecf9bb39f4f6df21ebc66546ac6a51b998163a0
Сергей <goroxov.99@mail.ru>
2023-12-09 09:28:47 +0300

    Реализован CRUD для собак

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from dogs.views import index, BreedView, DogView, DogCreateView, DogDetailView
+from dogs.views import index, BreedView, DogView, DogCreateView, DogDetailView, DogUpdateView, DogDeleteView
 from dogs.apps import DogsConfig
 
 app_name = DogsConfig.name
@@ -10,4 +10,6 @@ urlpatterns = [
     path("dogs/<int:pk>/", DogView.as_view(), name="dogs_breeds"),
     path("dogs/view/<int:pk>/", DogDetailView.as_view(), name="dogs_view"),
     path("dogs/create/", DogCreateView.as_view(), name="dogs_create"),
+    path("dogs/update/<int:pk>/", DogUpdateView.as_view(), name="dogs_update"),
+    path("dogs/delete/<int:pk>/", DogDeleteView.as_view(), name="dogs_delete"),
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from dogs.models import Dog, Breed
 
@@ -24,8 +24,10 @@ class DogView(ListView):
         queryset = queryset.filter(breed__pk=self.kwargs.get("pk"))
         return queryset
 
+
 class DogDetailView(DetailView):
     model = Dog
+
 
 class DogCreateView(CreateView):
     model = Dog
@@ -33,3 +35,16 @@ class DogCreateView(CreateView):
     success_url = reverse_lazy("dogs:breeds")
 
 
+class DogUpdateView(UpdateView):
+    model = Dog
+    fields = ("name", "breed", "photo", "birth_day")
+
+    def get_success_url(self):
+        return reverse("dogs:dogs_breeds", args=[self.object.breed.pk])
+
+
+class DogDeleteView(DeleteView):
+    model = Dog
+
+    def get_success_url(self):
+        return reverse("dogs:dogs_breeds", args=[self.object.breed.pk])
